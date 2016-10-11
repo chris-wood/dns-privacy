b51cb9a923f02662313ab50018f79830786fddb7
Christopher Wood <christopherwood07@gmail.com>
2016-10-10 21:10:00 -0700

    Changes

--- scripts/pcap_parser.py
+++ scripts/pcap_parser.py
@@ -70,9 +70,10 @@ class DNSPacket(object):
             return False
 
         self.ip = self.ethernetPacket.data
-        #if (ipv4 and self.ip.p == dpkt.ip.IP_PROTO_UDP) or (not ipv4 and self.ip.nxt == dpkt.ip.IP_PROTO_UDP):
-        if True:
+        if (ipv4 and self.ip.p == dpkt.ip.IP_PROTO_UDP) or (not ipv4 and self.ip.nxt == dpkt.ip.IP_PROTO_UDP):
             #print "ok"
+            if type(self.ip) == type(""):
+                return False
             self.udp = self.ip.data
 
             tb = None
